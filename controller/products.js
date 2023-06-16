aec9cc3ba43ad8993626d72aac5838329fa6b506
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-06-17 03:30:04 +0500

    main

--- controller/products.js
+++ controller/products.js
@@ -4,8 +4,11 @@ import productsModel from "../model/products.js";
 const getProducts = async (req, res) => {
   try {
     const products = await productsModel.find().exec();
-    console.log("get req")
-    res.status(200).json(products);
+    const items = products.map((product) => ({
+      ...product._doc,
+      image: "http://localhost:8000/" + product.image,
+    }));
+    res.status(200).json(items);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch products" });
@@ -17,13 +20,16 @@ const getProductById = async (req, res) => {
   try {
     const productId = req.params.id;
     const product = await productsModel.findById(productId);
-    
-    console.log("Get product by ID successful");
+    const item = {
+      ...product._doc,
+      image: "http://localhost:8000/" + product.image,
+    };
+
     if (!product) {
       return res.status(404).json({ error: "Product not found" });
     }
 
-    res.status(200).json(product);
+    res.status(200).json(item);
     console.log("Get product by ID successful");
   } catch (error) {
     console.error(error);
