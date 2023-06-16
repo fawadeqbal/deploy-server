1a691d8ea54fb3da43f56e0fae99bff392cbe37d
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-06-17 01:39:58 +0500

    main

--- multer_config/multer_config.js
+++ multer_config/multer_config.js
@@ -1,11 +1,16 @@
 import multer from 'multer';
 import multerS3 from 'multer-s3';
 import AWS from 'aws-sdk';
-
+const AWS_REGION="ap-south-1"
+const AWS_ACCESS_KEY_ID="ASIAUE5ROX7WBPG7W5EE"
+const AWS_SECRET_ACCESS_KEY="Co4TjmAhMNfPTYZ2d6PDvU3JrQ5H/avHaAdKsD1e"
 // Configure the AWS SDK with your credentials
 AWS.config.update({
-  region: 'REGION',
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  region: AWS_REGION,
 });
+
 
 const s3 = new AWS.S3();
 const upload = multer({
