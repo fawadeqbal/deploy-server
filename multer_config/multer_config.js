a0c04987b1908a64abe74872ba86c6413275927e
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-06-17 01:28:45 +0500

    main

--- multer_config/multer_config.js
+++ multer_config/multer_config.js
@@ -1,24 +1,25 @@
-import multer from "multer";
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import AWS from 'aws-sdk';
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./upload/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, new Date().toISOString() + file.originalname);
-  },
+// Configure the AWS SDK with your credentials
+AWS.config.update({
+  region: 'REGION',
 });
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
 
+const s3 = new AWS.S3();
 const upload = multer({
-  storage: storage,
-  fileFilter: fileFilter,
+  storage: multerS3({
+    s3: s3,
+    bucket: 'upload',
+    acl: 'public-read',
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      cb(null, Date.now().toString() + '-' + file.originalname);
+    },
+  }),
 });
 
-export { upload };
+export {upload}
